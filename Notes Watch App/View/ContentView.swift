f9a0897226fd8a9aec11a9aed375406bb0e389d5
Ivan Wilhelm (ele/dele) <ivan.wilhelm@bv.com.br>
2023-06-15 17:34:55 -0300

    Creating the list items view

--- Notes Watch App/View/ContentView.swift
+++ Notes Watch App/View/ContentView.swift
@@ -7,8 +7,38 @@ struct ContentView: View {
   @State private var text: String = ""
   
   // MARK: - FUNCTIONS
-  func save() {
-    dump(notes)
+  private func save() {
+    do {
+      let data = try JSONEncoder().encode(notes)
+      let url = getDocumentDirectory().appendingPathComponent("notes")
+      try data.write(to: url)
+    } catch {
+      print("Saving data has failed.")
+    }
+  }
+  
+  private func load() {
+    DispatchQueue.main.async {
+      do {
+        let url = getDocumentDirectory().appendingPathComponent("notes")
+        let data = try Data(contentsOf: url)
+        notes = try JSONDecoder().decode([Note].self, from: data)
+      } catch {
+        print("Loading data has failed.")
+      }
+    }
+  }
+  
+  private func delete(offsets: IndexSet) {
+    withAnimation {
+      notes.remove(atOffsets: offsets)
+      save()
+    }
+  }
+  
+  private func getDocumentDirectory() -> URL {
+    let path = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)
+    return path[0]
   }
   
   // MARK: - BODY
@@ -22,10 +52,9 @@ struct ContentView: View {
             guard text.isEmpty == false else { return }
             
             let note: Note = Note(id: UUID(), text: text)
+
             notes.append(note)
-            
             text = ""
-            
             save()
           } label: {
             Image(systemName: "plus.circle")
@@ -38,9 +67,39 @@ struct ContentView: View {
         
         Spacer()
         
-        Text("\(notes.count)")
+        if notes.count >= 1 {
+          List {
+            ForEach(0..<notes.count, id:\.self) { i in
+              HStack {
+                Capsule()
+                  .frame(width: 4)
+                  .foregroundColor(.accentColor)
+                
+                Text(notes[i].text)
+                  .lineLimit(1)
+                  .padding(.leading, 5)
+              } //: HStack
+            } //: ForEach
+            .onDelete(perform: delete)
+          } //: List
+        } else {
+          Spacer()
+          
+          Image(systemName: "note.text")
+            .resizable()
+            .scaledToFit()
+            .foregroundColor(.gray)
+            .opacity(0.25)
+            .padding(25)
+          
+          Spacer()
+        }
       } //: VStack
       .navigationTitle("Notes")
+      .navigationBarTitleDisplayMode(.inline)
+      .onAppear(perform: {
+        load()
+      })
     } //: NavigationStack
   } //: Body
 }
